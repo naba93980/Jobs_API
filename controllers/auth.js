af1209393e3112cc6884166ef301708102efe6ba
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-25 02:58:22 +0530

    called doc instance method to check password

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,18 +2,22 @@ const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
 
+// register controller function
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
+  // check is email, name, password is present 
   if (!name || !email || !password) {
     throw new BadRequestError("please put all name, email,pasword");
   }
 
+  // create an account in database and send jwt token
   const user = await User.create({ ...req.body });
   const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
+// login controller function
 const login = async (req, res) => {
 
   const { email, password } = req.body;
@@ -21,10 +25,19 @@ const login = async (req, res) => {
     throw new BadRequestError("Please provide email and password");
   }
 
+  // check if email exist in database
   const user = await User.findOne({ email });
   if (!user) {
-    throw new UnauthenticatedError("Invalid Credentials");
+    throw new UnauthenticatedError("Invalid email");
   }
+
+  // check if password is correct
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid password');
+  }
+
+  // if both email and password are valid, user is logged in and jwt is sent as response
   const token = user.createJWT();
   res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
