470ac77d5d723aeb6fbf3d0abe69ee9a807b1166
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-24 22:30:52 +0530

    added document middleware and instance method

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -20,8 +22,25 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
-    maxlength: 12,
+    maxlength : 12
   },
 });
+
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+})
+
+// user defined instance methods of document
+// UserSchema.methods.getName = function () {
+//   return this.name;
+// }
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES
+  });
+}
 
 module.exports = mongoose.model('UserCollection', UserSchema);
