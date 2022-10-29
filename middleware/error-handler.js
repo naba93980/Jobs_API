ba261b205be1d6b6fa5675f7d50c9908021ec022
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-29 20:26:40 +0530

    created a custom error object and chnages error code based on certain condition

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,34 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  console.log(err);
+
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later',
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id : ${err.value}, check id length ...\n ${err.reason}`
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg })
+
 }
 
 module.exports = errorHandlerMiddleware
